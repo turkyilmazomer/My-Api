5003d0e7ad9d3933d5bca45fce32f34136a78eb4
oturkyilmaz <omer.turkyilmaz@hepsiburada.com>
2020-12-22 12:52:36 +0300

    files edited

--- app/main.go
+++ app/main.go
@@ -1,11 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo"
+	"github.com/spf13/viper"
+)
 
 func init() {
+	viper.SetConfigFile(`..\config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 
+	if viper.GetBool(`debug`) {
+		fmt.Println("Service RUN on DEBUG mode")
+	}
+}
+
+/*
+ * Tag... - a very simple struct
+ */
+type Tag struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
 }
 
 func main() {
-	fmt.Println("Ömer Türkyılmaz")
+
+	fmt.Println("Go MySQL Tutorial")
+
+	dbHost := viper.GetString(`database.host`)
+	dbPort := viper.GetString(`database.port`)
+	dbUser := viper.GetString(`database.user`)
+	dbPass := viper.GetString(`database.pass`)
+	dbName := viper.GetString(`database.name`)
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	dbConn, err := sql.Open("mysql", connection)
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = dbConn.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		err := dbConn.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	e := echo.New()
+	middL := _articleHttpDeliveryMiddleware.InitMiddleware()
+	e.Use(middL.CORS)
+	authorRepo := _authorRepo.NewMysqlAuthorRepository(dbConn)
+	ar := _articleRepo.NewMysqlArticleRepository(dbConn)
+
+	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
+	au := _articleUcase.NewArticleUsecase(ar, authorRepo, timeoutContext)
+	_articleHttpDelivery.NewArticleHandler(e, au)
+
+	log.Fatal(e.Start(viper.GetString("server.address")))
+
+	//results, err := dbConn.Query("SELECT id, title FROM article")
+	//if err != nil {
+	//	panic(err.Error()) // proper error handling instead of panic in your app
+	//}
+
+	//for results.Next() {
+	//	var tag Tag
+	// for each row, scan the result into our tag composite object
+	//	err = results.Scan(&tag.ID, &tag.Title)
+	//	if err != nil {
+	//		panic(err.Error()) // proper error handling instead of panic in your app
+	//	}
+	// and then print out the tag's Name attribute
+	//	log.Printf(tag.Title)
+	//}
+
 }
---
+++ go.mod
@@ -0,0 +1,10 @@
+module My-Api
+
+go 1.15
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/spf13/viper v1.7.1
+)
